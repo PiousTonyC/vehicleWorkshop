5f6619da7a4ccc4e18cdad1d9bace9f7a3a16c82
milanjijo <milanjijo2k02@gmail.com>
2022-11-23 08:45:11 +0530

    Garage Func Added

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,13 +1,16 @@
 import './sidebar.css'
 
-const Sidebar = () => {
+const Sidebar = ({toggle,setToggle}) => {
+
+    
     return (
         <div className="sideBarContainer">
-            <div className="elements" id='dashboard'>Dashboard</div>
-            <div className="elements" id='myprofile'>My Profile</div>
-            <div className="elements" id='addvehicles'>Add Vehicles</div>
-            <div className="elements" id='bookservices'>Book Service</div>
-            <div className="elements" id='checkstatus'>Check Status</div>
+
+            <div className="elements" id='dashboard' onClick={()=>setToggle(1)}>Dashboard</div>
+            <div className="elements" id='myprofile' onClick={()=>setToggle(2)}>My Profile</div>
+            <div className="elements" id='addvehicles' onClick={()=>setToggle(3)}>Add Vehicles</div>
+            <div className="elements" id='bookservices' onClick={()=>setToggle(4)}>Book Service</div>
+            <div className="elements" id='checkstatus' onClick={()=>setToggle(5)}>Check Status</div>
         </div>
     );
 }
--- src/pages/garage/garage.js
+++ src/pages/garage/garage.js
@@ -4,20 +4,28 @@ import Dashboard from '../../components/dashboard/dashboard';
 import Myprofile from '../../components/myProfile/myProfile';
 import Addvehicle from '../../components/addvehicle/addvehicle';
 import Checkstatus from '../../components/checkstatus/checkstatus';
+import { useState } from 'react';
 
 const Garage = () => {
+    const [toggle,setToggle]=useState(1)
+
     return (
         <div className="garageContainer">
             <div className="grdContainer">
                 <div className="item1">
-                    <Sidebar/>
+                    <Sidebar toggle={toggle} setToggle={setToggle} />
                 </div>
                 <div className="item2">
                     {/* <p>button click cheyumbol dashboard my profile my vehicle book service check status okkey ivede render aaye veranam</p> */}
-                    {/* <Dashboard/> */}
-                    {/* <Myprofile/> */}
-                    {/* <Addvehicle/> */}
-                    <Checkstatus/>
+                    {console.log(toggle)}
+                    {toggle===1 && <Dashboard/>}
+                    {toggle===2 && <Myprofile/>} 
+                    {toggle===3 && <Addvehicle/>}
+                    {/* {toggle===4 && <Dashboard/>} */}
+                    {toggle===5 && <Checkstatus/>}
+                    
+                    
+                    
                 </div>
             </div>
         </div>
